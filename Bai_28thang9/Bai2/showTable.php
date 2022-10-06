5fce2cfbec2ab0c8c676b6d32e5c9a2f3ccbdae9
quixun <xuanphan742@gmail.com>
2022-10-06 15:57:58 +0700

    Ngay6_10

---
+++ Bai_28thang9/Bai2/showTable.php
@@ -0,0 +1,11 @@
+﻿<?php 
+$op=$_GET["chon"];
+if($op!=""){
+    include("database.class.php");
+    $dao=new Dao("root","","udn");
+
+    $sql="SELECT * from {$op}";
+    $header="DANH SÁCH {$op}";
+    $dao->table($sql,$header);
+}
+?>
